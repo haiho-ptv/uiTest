dbf9e011eab31137fd30ed6879fac5c5448c60ba
Hai Ho <hai.ho@ptvltd.com>
2024-11-27 16:15:28 +0700

    init project

---
+++ uiTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uiTest
+//
+//  Created by PTV on 27/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
