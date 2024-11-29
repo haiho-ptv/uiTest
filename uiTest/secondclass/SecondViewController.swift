4ada42229b7396c0f3be95fb19aaceb733a973bb
Hai Ho <hai.ho@ptvltd.com>
2024-11-29 10:21:10 +0700

    Action back from another class

---
+++ uiTest/secondclass/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  uiTest
+//
+//  Created by PTV on 29/11/2024.
+//
+
+import UIKit
+ 
+class SecondViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
