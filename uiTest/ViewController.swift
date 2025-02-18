8391f5b60934491dfbb70200f8df36ff6ab4f13d
Hai Ho <hai.ho@ptvltd.com>
2025-02-18 17:28:15 +0700

    simple UI

--- uiTest/ViewController.swift
+++ uiTest/ViewController.swift
@@ -9,16 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var logonImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func backToHome(segue : UIStoryboardSegue){
-        
-    }
 
 }
 
